a7d61c02da70bb8dcbb5101b7de5aa6db1d79ac7
unknown <giovannyjchoquecenteno@gmail.com>
2022-12-08 16:52:58 -0400

    Completando acerca de

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import empresaicon from '../assets/image/empresaicon.png';
 import fundacionicon from '../assets/image/fundacionicon.png';
 import lsbicon from '../assets/image/lsbicon.png';
-
+import {AiOutlineMail , AiOutlinePhone, AiOutlineGithub} from 'react-icons/ai'
 
 
 const About = () => {
   return ( 
     <div className="container mx-auto bg-base-100 p-5">
     <h2 className='text-center fredoka text-3xl mb-5'>Nustro Equipo</h2>
-    <div className='flex flex-wrap gap-4 justify-center sm:justify-between'>
-
-        <a href="https://hands-sign.netlify.app/" target="_blank">
+    <div className='flex flex-col sm:flex-row gap-4 justify-around'>
+        <a  href="https://hands-sign.netlify.app/" target="_blank">
             <div className="w-full rounded-md sm:w-80 bg-base-100 shadow-xl hover:transition-all hover:duration-150 hover:scale-95 cursor-pointer">
                 <figure><img src={lsbicon} className='rounded-md  w-full' alt="Shoes" /></figure>
                 <div className="card-body">
@@ -20,10 +19,25 @@ const About = () => {
                         Desarrollador Frontend con habilidades en HTML,CSS,Javascript y React.js
                     </p>
                 </div>
+                <hr />
+                <div className='card-actions p-5'>
+                    <div className='flex flex-row'>
+                    <AiOutlineMail size={25} />
+                    <span>giovannyjchoquecenteno@gmail.com</span>
+                    </div>
+                    <div className='flex flex-row'>
+                    <AiOutlinePhone size={25} />
+                    <span>79019549</span>
+                    </div>
+                    <div className='flex flex-row'>
+                    <AiOutlineGithub size={25} />
+                    <span>GiovannyChoqueCenteno</span>
+                    </div>
+                </div>
             </div>
         </a>
 
-        <a href="https://www.w3schools.com" target="_blank">
+        <a  href="https://www.w3schools.com" target="_blank">
             <div className="w-full rounded-md sm:w-80 bg-base-100 shadow-xl hover:transition-all hover:duration-150 hover:scale-95 cursor-pointer">
                 <figure><img src={fundacionicon} className='rounded-md w-full' alt="Shoes" /></figure>
                 <div className="card-body">
@@ -32,10 +46,23 @@ const About = () => {
                         Desarrolador Backend y Movil con habilidades en JS,Node.js y React Native
                     </p>
                 </div>
+                <hr />
+                <div className='card-actions flex-col p-5'>
+                    <div className='flex flex-row'>
+                    <AiOutlineMail size={25} />
+                    <span>jorge.ecrg@gmail.com</span>
+                    </div>
+                    <div className='flex flex-row'>
+                    <AiOutlinePhone size={25} />
+                    <span>72602365</span>
+                    </div>
+                    <div className='flex flex-row'>
+                    <AiOutlineGithub size={25} />
+                    <span>EduardoRodriguez</span>
+                    </div>
+                </div>
             </div>
         </a>
-
-
     </div>
 </div>
 
